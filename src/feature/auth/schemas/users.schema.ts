9d9a7f091d23ffef0d2eb5de5ff6f8ea117eccdf
LukaTypeScripter <lukashinjikashvili85@gmail.com>
2026-04-18 00:12:02 +0400

    refactor(auth): update UsersSchema to use nullish for optional fields and improve type inference in LocalStrategy

--- src/feature/auth/schemas/users.schema.ts
+++ src/feature/auth/schemas/users.schema.ts
@@ -3,8 +3,8 @@ import z from 'zod';
 export const UsersSchema = z.object({
   email: z.string(),
   name: z.string(),
-  avatar: z.string().optional(),
-  password: z.string().optional(),
+  avatar: z.string().nullish(),
+  password: z.string().nullish(),
 });
 
 export type UsersType = z.infer<typeof UsersSchema>;
